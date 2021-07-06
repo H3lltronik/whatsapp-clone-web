454c5c38583bb435612e698d999414dd5e812687
H3lltronik <esau.egs1@gmail.com>
2021-07-05 23:38:30 -0500

    Basic layout

---
+++ src/utils/icons.ts
@@ -0,0 +1,87 @@
+import arrowdown from '../../static/icons/arrow-down.svg'
+import arrowleft from '../../static/icons/arrow-left.svg'
+import block from '../../static/icons/block.svg'
+import camera from '../../static/icons/camera.svg'
+import checktwo2 from '../../static/icons/check-two-2.svg'
+import checktwo3 from '../../static/icons/check-two-3.svg'
+import checktwo from '../../static/icons/check-two.svg'
+import check from '../../static/icons/check.svg'
+import clip from '../../static/icons/clip.svg'
+import computer from '../../static/icons/computer.svg'
+import cross from '../../static/icons/cross.svg'
+import dislike from '../../static/icons/dislike.svg'
+import doc from '../../static/icons/doc.svg'
+import gifborderless from '../../static/icons/gif-borderless.svg'
+import gif from '../../static/icons/gif.svg'
+import gif2 from '../../static/icons/gif2.svg'
+import group from '../../static/icons/group.svg'
+import iconcamera from '../../static/icons/icon-camera.svg'
+import icondoc from '../../static/icons/icon-doc.svg'
+import icongallery from '../../static/icons/icon-gallery.svg'
+import iconperson from '../../static/icons/icon-person.svg'
+import iconvideocall from '../../static/icons/icon-videocall.svg'
+import menu from '../../static/icons/menu.svg'
+import message from '../../static/icons/message.svg'
+import microphone from '../../static/icons/microphone.svg'
+import microphone2 from '../../static/icons/microphone2.svg'
+import mute from '../../static/icons/mute.svg'
+import noprofilepic from '../../static/icons/no-profile-pic.svg'
+import notifications from '../../static/icons/notifications.svg'
+import reload from '../../static/icons/reload.svg'
+import rightarrow from '../../static/icons/right-arrow.svg'
+import search from '../../static/icons/search.svg'
+import search2 from '../../static/icons/search2.svg'
+import share from '../../static/icons/share.svg'
+import smiley from '../../static/icons/smiley.svg'
+import status from '../../static/icons/status.svg'
+import status2 from '../../static/icons/status-empty.svg'
+import sticker from '../../static/icons/sticker.svg'
+import tabremitent from '../../static/icons/tab-remitent.svg'
+import tabsender from '../../static/icons/tab-sender.svg'
+import trash from '../../static/icons/trash.svg'
+import videocamera from '../../static/icons/videocamera.svg'
+
+export default {
+    arrowdown,
+    arrowleft,
+    block,
+    camera,
+    checktwo2,
+    checktwo3,
+    checktwo,
+    check,
+    clip,
+    computer,
+    cross,
+    dislike,
+    doc,
+    gifborderless,
+    gif,
+    gif2,
+    group,
+    iconcamera,
+    icondoc,
+    icongallery,
+    iconperson,
+    iconvideocall,
+    menu,
+    message,
+    microphone,
+    microphone2,
+    mute,
+    noprofilepic,
+    notifications,
+    reload,
+    rightarrow,
+    search,
+    search2,
+    share,
+    smiley,
+    status,
+    status2,
+    sticker,
+    tabremitent,
+    tabsender,
+    trash,
+    videocamera,
+}
